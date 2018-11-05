bfa35a329f830dbfcf8053c3d0ae70b1e1301ae7
Dezzi94 <techidesmond@gmail.com>
2018-11-05 20:04:37 +0000

    Add files via upload

---
+++ Simple Program/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Simple_Program
+{
+    partial class frmButtonsAndMessageboxes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRed.Location = new System.Drawing.Point(76, 44);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(157, 43);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red Button";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlue.Location = new System.Drawing.Point(76, 155);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(157, 43);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "Blue Button";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // frmButtonsAndMessageboxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 261);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "frmButtonsAndMessageboxes";
+            this.Text = "Buttons Program";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+    }
+}
+
---
+++ Simple Program/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Program
+{
+    public partial class frmButtonsAndMessageboxes : Form
+    {
+        public frmButtonsAndMessageboxes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            //This shows a messagebox to the user when the Red button is clicked.
+            MessageBox.Show("You clicked on the Red button.", "Red Button");
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {   
+            //This shows a messagebox to the user when the Blue button is clicked.
+            MessageBox.Show("You clicked on the Blue button.", "Blue Button");
+        }
+    }
+}
